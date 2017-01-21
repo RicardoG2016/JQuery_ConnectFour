a39cbf0a49d86eff5d163c5b49c67e27ce38311d
RicardoG2016 <gomez.r@live.com>
2017-01-21 17:53:47 -0600

    can draw circles and identify specific cells and pass arguments to functions needed to draw circle

--- js/game.js
+++ js/game.js
@@ -1,21 +1,49 @@
-
 $(document).ready(function(){  
   var turnCount = 0;
 
-  var button = [];
-  for (var i = 1; i < 43; i++) {
-    button[i] = $('canvas') + (i);
-  }; 
-  
-  var bDisabled = false;
-  for (var i = 1; i < 43; i++) bDisabled[i] = false; 
+  // var bDisabled = false;
+  // for (var i = 1; i < 43; i++) bDisabled[i] = false;
 
   var gameResult = false;
   var content = [];
+  var num
 
-  function loop(x){
-    if(bDisabled == false){
-      console.log('works');
+  var button = [];
+  for (var i = 1; i < 43; i++) button[i] = document.getElementById('canvas'+i);
+
+  var ctx = [];
+  for (var i = 1; i < 43; i++) ctx[i] = button[i].getContext("2d");
+
+  $( "canvas" ).on( "click", function(){
+    var cell = (this);
+    var num = cell.id.match(/\d/g).join("");
+    var color = 'green';
+    Dcircle(cell, num, color);
+    console.log(num);
+  });
+
+// draws the circle
+  function Dcircle(cell, num, color){
+    console.log(num);
+    ctx[num] = cell.getContext('2d');
+    ctx[num].beginPath();
+    ctx[num].lineWidth = 5;
+    ctx[num].arc(25,25,17,0,Math.PI*2,false);
+    ctx[num].fillStyle = color;
+    ctx[num].fill();
+    ctx[num].strokeStyle = color;
+    ctx[num].stroke();
+    ctx[num].closePath();
+    cell.style.opacity = 0.7;
+  };
+
+
+
+
+
+    // function loop(x){
+    // if(bDisabled == false){
+    //   console.log('works');
     //   bDisabled == true;
     //   button[x].style.opacity = 0.7;
 
@@ -32,7 +60,7 @@ $(document).ready(function(){
     //   ctx[x].arc(25,25,17,0,Math.PI*2,false);
     //   ctx[x].stroke();
     //   ctx[x].closePath();
-    };
-  };
+  //   };
+  // };
 });
 
