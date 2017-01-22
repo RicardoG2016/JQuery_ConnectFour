c5a6445ecf1043115b5c5f191c9a05192d913c69
RicardoG2016 <gomez.r@live.com>
2017-01-21 20:53:32 -0600

    connect four game works. Player and computer can each have a turn. Computer is randomly selected while player can choose a cell that doesn't have a class of disabled.

--- js/game.js
+++ js/game.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){  
   var turnCount = 0;
-
-  // var bDisabled = false;
-  // for (var i = 1; i < 43; i++) bDisabled[i] = false;
-
   var gameResult = false;
   var content = [];
-  var num
 
   var button = [];
   for (var i = 1; i < 43; i++) button[i] = document.getElementById('canvas'+i);
@@ -14,53 +9,57 @@ $(document).ready(function(){
   var ctx = [];
   for (var i = 1; i < 43; i++) ctx[i] = button[i].getContext("2d");
 
+// player turn and initial start to game
   $( "canvas" ).on( "click", function(){
     var cell = (this);
     var num = cell.id.match(/\d/g).join("");
     var color = 'green';
-    Dcircle(cell, num, color);
-    console.log(num);
+    if ($(cell).not('.bDisabled')){
+      ($(cell).addClass('bDisabled'))
+      content[num] = 'x';
+      cell.style.webkitTransform = "rotateY(180deg)";
+      dCircle(button, num, color);    
+    }
+    // checkWinner();
+    if (gameResult == false && turnCount < 42){
+      computerTurn(button);
+    };
   });
 
-// draws the circle
-  function Dcircle(cell, num, color){
-    console.log(num);
-    ctx[num] = cell.getContext('2d');
-    ctx[num].beginPath();
-    ctx[num].lineWidth = 5;
-    ctx[num].arc(25,25,17,0,Math.PI*2,false);
-    ctx[num].fillStyle = color;
-    ctx[num].fill();
-    ctx[num].strokeStyle = color;
-    ctx[num].stroke();
-    ctx[num].closePath();
-    cell.style.opacity = 0.7;
+// draws the circle function
+  function dCircle(button, num, color){
+    turnCount ++;
+    button[num].style.opacity = 0.7;
+// delays drawing of circle below
+    setTimeout(function(){
+      ctx[num].beginPath();
+      ctx[num].lineWidth = 5;
+      ctx[num].arc(25,25,17,0,Math.PI*2,false);
+      ctx[num].fillStyle = color;
+      ctx[num].fill();
+      ctx[num].strokeStyle = color;
+      ctx[num].stroke();
+      ctx[num].closePath();  
+    },300); 
   };
 
+// Computer basic logic below
+function computerTurn(button){
+  content[cpu] = 'y';
+  var cpu = Math.floor(Math.random()*42);
+  var color = 'red';
+  if ($('button[cpu]').not('.bDisabled')){
+    dCircle(button, cpu, color);
+    $('button[cpu]').addClass('bDisabled');
+  }
+  else computerTurn(button);
+};
+
+// function checkWinner(){
+//   console.log( ($('canvas').filter('.bDisabled')) );
+// };
 
 
-
-
-    // function loop(x){
-    // if(bDisabled == false){
-    //   console.log('works');
-    //   bDisabled == true;
-    //   button[x].style.opacity = 0.7;
-
-    //   content[x] = 'x';
-    //   turnCount ++;
-
-    // button[x].style.webkitTransform = "rotateY(180deg)"; 
-    // button[x].style.msTransform = "rotateY(180deg)"; 
-    // button[x].style.mozTransform = "rotateY(180deg)"; 
-    // button[x].style.oTransform = "rotateY(180deg)"; 
-
-    //   ctx[x].beginPath();
-    //   ctx[x].lineWidth = 2;
-    //   ctx[x].arc(25,25,17,0,Math.PI*2,false);
-    //   ctx[x].stroke();
-    //   ctx[x].closePath();
-  //   };
-  // };
+// closes doc ready below
 });
 
